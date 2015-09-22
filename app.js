cb3b1abb202f71c523bbb19d7a585abf2cc47b77
jashelton <justin.shelton@yahoo.com>
2015-09-22 08:01:18 -0400

    fixing app.js for serve

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var app = require("./server/routes");
 
 // Start the server
-var server = app.listen(8000, function() {
- console.log('Listening on port %d', server.address().port);
+// var server = app.listen(8000, function() {
+//  console.log('Listening on port %d', server.address().port);
+port = process.env.PORT || 8000;
+
+var server = app.listen(port, function() {
+console.log("Listening on " + port);
 });
